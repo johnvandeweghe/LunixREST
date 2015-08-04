1ec5484bbf9cdb0844b3f2cd76bea4642ea5cb7b
John VanDeWeghe <vandeweghe@lunixlabs.com>
2015-08-04 01:28:32 -0500

    Change handle to handle Requests. Update sample.

--- Router/Router.php
+++ Router/Router.php
@@ -10,63 +10,61 @@ use LunixREST\Exceptions\UnknownResponseFormatException;
 use LunixREST\Request\Request;
 
 class Router {
-    protected $request;
     protected $endPointNamespace;
     protected $accessControl;
     protected $outputConfig;
     protected $formatsConfig;
 
-    public function __construct(Request $request, AccessControl $accessControl,  Configuration $outputConfig, Configuration $formatsConfig, $endPointNamespace = ''){
+    public function __construct(AccessControl $accessControl,  Configuration $outputConfig, Configuration $formatsConfig, $endPointNamespace = ''){
         $this->endPointNamespace = $endPointNamespace;
-        $this->request = $request;
         $this->accessControl = $accessControl;
         $this->outputConfig = $outputConfig;
         $this->formatsConfig = $formatsConfig;
     }
 
-    public function handle(){
-        if(!$this->validateExtension()){
-            throw new UnknownResponseFormatException('Unknown response format: ' . $this->request->getExtension());
+    public function handle(Request $request){
+        if(!$this->validateExtension($request)){
+            throw new UnknownResponseFormatException('Unknown response format: ' .$request->getExtension());
         }
 
-        $fullEndPoint = '\\' . $this->endPointNamespace . '\EndPoints\\' . $this->request->getEndPoint();
+        $fullEndPoint = '\\' . $this->endPointNamespace . '\EndPoints\\' . $request->getEndPoint();
         if(!class_exists($fullEndPoint)){
             throw new UnknownEndPointException("unknown endpoint: " . $fullEndPoint);
         }
 
-        if(!$this->validateEndpoint()){
+        if(!$this->validateEndpoint($request)){
             throw new UnknownEndPointException("unknown endpoint: " . $fullEndPoint);
         }
 
-        if(!$this->APIKeyAccessible($this->request->getApiKey(), $this->request->getEndPoint(), $this->request->getMethod(), $this->request->getInstance())){
+        if(!$this->APIKeyAccessible($request->getApiKey(), $request->getEndPoint(), $request->getMethod(), $request->getInstance())){
             throw new AccessDeniedException("API key does not have the required permissions to access requested resource");
         }
 
-        $endPoint = new $fullEndPoint($this->request);
-        $responseData = $endPoint->{$this->request->getMethod()}($this->request->getInstance(), $this->request->getData());
+        $endPoint = new $fullEndPoint($request);
+        $responseData = $endPoint->{$request->getMethod()}($request->getInstance(), $request->getData());
 
-        $responseClass = '\\LunixREST\\Response\\' . strtoupper($this->request->getExtension()) . "Response";
+        $responseClass = '\\LunixREST\\Response\\' . strtoupper($request->getExtension()) . "Response";
         $format = new $responseClass($responseData);
-        if(!$format->validate($this->outputConfig, $this->request->getMethod(), $this->request->getEndPoint(), $this->request->getVersion())){
+        if(!$format->validate($this->outputConfig, $request->getMethod(), $request->getEndPoint(), $request->getVersion())){
             throw new InvalidResponseFormatException('Method output did not match defined output format');
         }
 
         return $format->output();
     }
 
-    private function validateEndpoint()
+    private function validateEndpoint(Request $request)
     {
-        $formats = $this->formatsConfig->get('endpoints_' . str_replace('.', '_', $this->request->getVersion()));
-        return $formats && in_array($this->request->getEndpoint(), $formats);
+        $formats = $this->formatsConfig->get('endpoints_' . str_replace('.', '_', $request->getVersion()));
+        return $formats && in_array($request->getEndpoint(), $formats);
     }
 
     private function APIKeyAccessible($apiKey, $endPoint, $method, $instance){
         return $this->accessControl->validate($apiKey, $endPoint, $method, $instance);
     }
 
-    private function validateExtension()
+    private function validateExtension(Request $request)
     {
         $formats = $this->formatsConfig->get('formats');
-        return $formats && in_array($this->request->getExtension(), $formats);
+        return $formats && in_array($request->getExtension(), $formats);
     }
 }
--- examples/helloworld/index.php
+++ examples/helloworld/index.php
@@ -3,22 +3,31 @@ require("vendor/autoload.php");
 
 require("helloworld.php");
 
-$request = new \LunixREST\Request\Request("GET", [], "/1.0/123456/helloworld.json", []);//new \LunixREST\Request\Request($_SERVER['REQUEST_METHOD'], getallheaders(), $_SERVER['REQUEST_URI'], $_REQUEST);
 $accessControl = new \LunixREST\AccessControl\AllAccessINIAccessControl("config/api_keys.ini");
 $outputConfig = new \LunixREST\Configuration\INIConfiguration("config/output.ini");
 $formatsConfig = new \LunixREST\Configuration\INIConfiguration("config/formats.ini");
+$router = new \LunixREST\Router\Router($accessControl, $outputConfig, $formatsConfig, "Sample");
 
 try {
-	$router = new \LunixREST\Router\Router($request, $accessControl, $outputConfig, $formatsConfig, "Sample");
-	echo $router->handle();
-} catch(\LunixREST\Exceptions\UnknownEndPointException $e){
-	header('404 Not Found', 404);
-} catch(\LunixREST\Exceptions\UnknownResponseFormatException $e){
-	header('404 Not Found', 404);
-} catch(\LunixREST\Exceptions\AccessDeniedException $e){
-	header('403 Access Denied', 403);
-} catch(\LunixREST\Exceptions\InvalidResponseFormatException $e){
-	header('500 Internal Server Error', 500);
+	$request = new \LunixREST\Request\Request("GET", [], "/1.0/123456/helloworld.json", []);//new \LunixREST\Request\Request($_SERVER['REQUEST_METHOD'], getallheaders(), $_SERVER['REQUEST_URI'], $_REQUEST);
+
+	try {
+		echo $router->handle($request);
+	} catch(\LunixREST\Exceptions\InvalidRequestFormatException $e){
+		header('400 Bad Request', 400);
+	} catch(\LunixREST\Exceptions\UnknownEndPointException $e){
+		header('404 Not Found', 404);
+	} catch(\LunixREST\Exceptions\UnknownResponseFormatException $e){
+		header('404 Not Found', 404);
+	} catch(\LunixREST\Exceptions\AccessDeniedException $e){
+		header('403 Access Denied', 403);
+	} catch(\LunixREST\Exceptions\InvalidResponseFormatException $e){
+		header('500 Internal Server Error', 500);
+	} catch(Exception $e){
+		header('500 Internal Server Error', 500);
+	}
+} catch(\LunixREST\Exceptions\InvalidRequestFormatException $e){
+	header('400 Bad Request', 400);
 } catch(Exception $e){
 	header('500 Internal Server Error', 500);
 }
